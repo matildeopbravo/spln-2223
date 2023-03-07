df451b446c9d0454656220815cc914ba0a411bc8
matildeopbravo <matildeopbravo@gmail.com>
2023-03-07 01:45:24 +0000

    Added tpc2

--- TPC2/lex.py
+++ TPC2/lex.py
@@ -1,55 +1,48 @@
 import ply.lex as lex
 
-literals = [":", "-", "+", "$"]
+# LEXER
 
-tokens = ("ID", "ID_LING", "VAL", "LINHA_B", "NEWLINE")
+tokens = (
+    "OPEN_FULL",
+    "TEXT",
+    "INDEX",
+    "SEPARATOR",
+    "OPEN_CAT",
+    "OPEN_SIN",
+    "OPEN_VAR",
+    "OPEN_LANG",
+    "OPEN_NOTE",
+    "OPEN_REM",
+    "OPEN_VID",
+    "NEWLINE",
+    "SEP_ENTRADA",
+    "LANG",
+)
 
+w_c = r"A-Za-zÀ-ÖØ-öø-ÿ"
+literals = ["[", "]", ":"]
+t_ignore = " "
 
-# pode ser definição
-def t_ID(t):
-    r"\w+(?=:)"
-    return t
-
-
-def t_ID_LING(t):
-    r"(en|pt|es|la|ga)(?=:)"
-
-
-def t_LINHA_B(t):
-    r"\n\n"
-    return t
-
-
-def t_VAL(t):
-    r"[\w ]+"
-    return t
-
-
-def t_NEWLINE(t):
-    r"\n(?!\n)"
-    return t
-
-
-# "def t_EOF(t):
-# "    r"\$"
-# "    return t
-
-
-t_ignore = "\t"
+t_OPEN_FULL = r">"
+t_TEXT = rf"(?<![{w_c}\[])[{w_c}]+(?:[ ]+[{w_c}]+)*(?![{w_c}:])"
+t_INDEX = r"\d+"
+t_SEPARATOR = r";"
+t_OPEN_CAT = r"cat:"
+t_OPEN_SIN = r"sin:"
+t_OPEN_VAR = r"var:"
+# t_OPEN_LANG = rf"(?<=\[)[{w_c}\.]+(?=\])"
+t_LANG = r"(en|pt|es|la|ga):"
+t_OPEN_NOTE = r"nota:"
+t_OPEN_REM = r"\#"
+t_OPEN_VID = r"vid:"
+t_NEWLINE = r"\n(?!\n)"
+t_SEP_ENTRADA = r"\n\n"
 
 
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print(f"Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
+    t.lexer
 
 
 lexer = lex.lex()
-
-# lexer.input("id: 543\nNota: ola isto e uma nota\n")
-#
-# while True:
-#    tok = lexer.token()
-#    print(tok)
-#    if not tok:
-#        print("no tok")
-#        break
