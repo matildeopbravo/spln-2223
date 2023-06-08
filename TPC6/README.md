b9d68ca4914fc944f7820b7683eaddd29217d147
matildeopbravo <matildeopbravo@gmail.com>
2023-06-08 21:54:35 +0100

    Added README tpc6

--- TPC6/README.md
+++ TPC6/README.md
@@ -1,4 +1,23 @@
 # Web Scraping de Notícias
+
 Este trabalho consistiu no desenvolvimento de um *script* cujo objetivo é fazer
 *scraping* ao site de notícias do JN (Jornal de Notícias), com a utilização do
 *package* **newspaper3k**.
+
+## Procedimento
+O *script* desenvolvido cria uma pasta com o nome correspondente à data atual, se esta não existir. Similarmente, caso esta não exista,  irá criar a pasta `news` que
+    contém as pastas relativas a cada um dos dias em que foi executado. Será criado um ficheiro **xml** por todos os artigos obtidos, colocado na pasta correspondente ao dia em que executou. Além disso, também é criado um ficheiro de *log* que contém algumas informações sobre o processo.
+    O utilizador deverá especificar uma pasta onde será criada a pasta *news* e as restantes subpastas.
+
+## Execução Periódica
+Para executar o *script* de forma periódica, por exemplo, diariamente, poderá ser criada um *cron job*.
+
+Para abrir o ficheiro *crontab* dever-se-á executar o seguinte comando:
+```sh
+crontab -e
+```
+De seguida, deverá adicionar uma linha que indica o *script* a ser executado e a frequência com que este será executado.
+```sh
+	0 2 * * * path_to_script/scraper.py
+```
+Por exemplo, esta linha faria com que o *cron job* fosse executado todos os dias às 2 da manhã.
--- TPC6/scraper.py
+++ TPC6/scraper.py
@@ -1,33 +1,50 @@
 import newspaper
 import traceback
+import datetime
+import os
 import sys
 
+def create_daily_dir():
+    now = datetime.datetime.now().strftime('%Y-%m-%d')
+    dir_name = f"{articles_dir}/{now}"
+    os.makedirs(dir_name, exist_ok=True)
+    return dir_name
+
+def get_curr_time():
+    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def fetch_articles():
+    jn = newspaper.build(url)
+
+    log.write(f"[{get_curr_time()}] Fetched {jn.size()} articles\n")
+
+    for article in jn.articles:
+        ar = newspaper.Article(article.url)
+        log.write(f"[{get_curr_time()}] Downloading {ar.url}\n")
+        ar.download()
+
+        try:
+            ar.parse()
+            current_file = open(f"{day_dir}/{ar.title}.xml", "w")
+            print(f"{day_dir}/{ar.title}.xml")
+            current_file.write(
+                f"""<article>
+                        <title>{ar.title}</title>
+                        <url>{ar.url}</url>
+                        <author>{ar.authors}</author>
+                        <date>{ar.publish_date}</date>
+                        <text>{ar.text}</text>
+                    </article>
+            """)
+            log.write(f"[{get_curr_time()}] Saved {ar.url}\n")
+            current_file.close()
+        except Exception:
+            continue
+
+
 url = "https://www.jn.pt"
-jn = newspaper.build(url)
-xml_file = open("jn.xml", "w")
-xml_file.write("<articles>\n")
-
-print("Number of articles :", jn.size())
-
-for article in jn.articles:
-    print(article.url)
-    ar = newspaper.Article(article.url)
-    print(f"Downloading...")
-    ar.download()
-    ar.parse()
-    try:
-        xml_file.write(
-            f"""<article>
-                    <title>{ar.title}</title>
-                    <url>{ar.url}</url>
-                    <author>{ar.authors}</author>
-                    <date>{ar.publish_date}</date>
-                    <text>{ar.text}</text>
-                </article>
-        """
-        )
-    except Exception:
-        continue
-
-xml_file.write("<articles>")
-xml_file.close()
+articles_dir = f"{sys.argv[1]}/news"
+day_dir = create_daily_dir()
+log = open(f"{day_dir}/log.txt", "a")
+fetch_articles()
+log.close()
